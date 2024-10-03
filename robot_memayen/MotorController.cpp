df16a2d95fc0c51b718657ba8549b3b90e1b3416
shulme10 <danielshulem@gmail.com>
2024-10-03 22:35:49 +0300

    better looking

--- robot_memayen/MotorController.cpp
+++ robot_memayen/MotorController.cpp
@@ -26,8 +26,8 @@ void MotorController::setSpeed(const uint8_t speed) {
 
 void MotorController::moveAtAngle(const double angle) {
   double speedRatio = 1 - (abs(angle) / 90); //for example:  angle = 30 deg will give ratio of 0.66 between the 2 sides
-
   int16_t motorSpeed[4] = {defaultSpeed, defaultSpeed, -defaultSpeed, -defaultSpeed};
+  
   if(angle < 0){
     motorSpeed[0] *= speedRatio;
     motorSpeed[1] *= speedRatio;
@@ -37,14 +37,11 @@ void MotorController::moveAtAngle(const double angle) {
     motorSpeed[3] *= speedRatio;
   }
 
-  // update motor speed
+  // update motors
   for(int i = 0; i < 4; i++){
     analogWrite(motors[i].pwm, abs(motorSpeed[i]));
-  }
-
-  // activate motors
-  for(int i = 0; i < 4; i++){
-    digitalWrite(motors[i].in1, motorSpeed[i] >= 0);
-    digitalWrite(motors[i].in2, motorSpeed[i] < 0); // the opposite of the first pin, they both return a boolean where true = 1, false = 0.
+    bool direction = motorSpeed[i] >= 0;
+    digitalWrite(motors[i].in1, direction);
+    digitalWrite(motors[i].in2, !direction);
   }
 }
--- robot_memayen/robot_memayen.ino
+++ robot_memayen/robot_memayen.ino
@@ -12,15 +12,27 @@ void testDrive() {
   
   while(true){
     for(int i = 0; i < 4; i++){
+      motorController.resetMotors();
       digitalWrite(motors[i].in1, 1);
       digitalWrite(motors[i].in2, 0);
 
       serialPrintf("Activated pins: %u %u", motors[i].in1, motors[i].in2);
       delay(SECOND_MS);
     }
-    delay(SECOND_MS);
-    motorController.resetMotors();
-    delay(SECOND_MS);
+  }
+}
+
+void driveInFunnyAngle() {
+  double angle = 45;
+  while(true){
+    if(angle == -45) {
+      angle = 45;
+    }
+    else{
+      angle -= 5;
+    }
+    motorController.moveAtAngle(angle);
+    delay(100);
   }
 }
 
@@ -28,10 +40,9 @@ void testDrive() {
 void setup() {
   Serial.begin(9600);
   motorController.setup();
-  
+  testDrive()
 }
 
 void loop() {
-  testDrive();
 
 }
